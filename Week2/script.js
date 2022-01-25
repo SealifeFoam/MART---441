54a8716d9d93f143fb9f8afdbe398f490f630183
SealifeFoam <77304337+SealifeFoam@users.noreply.github.com>
2022-01-25 12:35:00 -0700

    W2

---
+++ Week2/script.js
@@ -0,0 +1,6 @@
+
+console.log("Favorite Websites- https://www.thisiscolossal.com/ https://dribbble.com/shots/popular/animation and https://mysterious.americanmcgee.com/");
+
+console.log("Favorite Games- Alice Madness Returns, Little Nightmares1and2, and Pokemon Soul Silver")
+
+console.log("Favorit Artist- Tony Babel because he's pushes the 1930s rubber hose style.  Alessandro Gallo’s for her impact on whats expected of Montanan artist and Marget Kean for her impact on art kiche and female artist.")
