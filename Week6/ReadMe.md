40a9ef4560e94d4a2019d981dbe665084c59b631
SealifeFoam <77304337+SealifeFoam@users.noreply.github.com>
2022-02-27 16:30:35 -0700

    Finally

---
+++ Week6/ReadMe.md
@@ -0,0 +1,5 @@
+2/23/2022 - I can't seem to figure out why my End page won't connect to the index page. 
+I had to delete the former images to focus on the current task at hand. (not that there was any to see)  
+
+2/26/2022 - it's always the simplest things - ALWAYS check your tags
+
---
+++ Week6/main.js
@@ -0,0 +1,140 @@
+// create an array of image names that correspond to the image tags
+var imageTags = ["image1", "image2", "image3", "image4" ];
+
+// Top/Blank Image- don't memory games usually have some sort of design on them though? 
+var blankImagePath = "Images/gofish.jpg";
+
+var firstNumber = -1;
+var secondNumber = -1;
+var score = 0;
+var allFound = 0;
+
+// JSON declaration
+var player = {"firstname":"", "lastname":"", "age":0, "score":0};
+
+//Array for the matching images
+var actualImages = new Array();
+
+
+
+
+function printBlanks()
+{
+   // call our random image creation function
+    createRandomImageArray();
+    //for loop
+    for(var i = 0; i < imageTags.length; i++)
+    {
+
+        document.getElementById(imageTags[i]).src= blankImagePath;
+    }
+       
+}
+
+function createRandomImageArray()
+{
+    
+    var actualImagePath = ["Images/dog.jpg","Images/tiger.jpg"];
+    var count = [0,0];
+   
+    while(actualImages.length < 4)
+    {
+        
+        var randomNumber = Math.floor(Math.random() * actualImagePath.length)
+          
+        if(count[randomNumber] < 2)
+        {
+            actualImages.push(actualImagePath[randomNumber]);
+            count[randomNumber] = count[randomNumber] + 1;
+        }
+    }
+    
+        
+}
+
+
+
+
+
+    function flipImage(number)
+    {
+        document.getElementById(imageTags[number]).src= actualImages[number]
+        // second image appear
+        if(firstNumber >= 0)
+        {
+            secondNumber = number;
+            document.getElementById(imageTags[number]).src = actualImages[secondNumber];
+            
+        }
+        else if(firstNumber < 0) // first image appear
+        {
+            firstNumber = number;
+            document.getElementById(imageTags[firstNumber]).src= actualImages[firstNumber];
+        
+        }
+    
+        // checking for if the images do not match
+        if(actualImages[secondNumber] != actualImages[firstNumber] && firstNumber >= 0 && secondNumber >= 0)
+        {
+            score++;
+            setTimeout(imagesDisappear, 1000); 
+        }
+        // checking to see if the images do match
+        else if(actualImages[secondNumber] == actualImages[firstNumber] && firstNumber >= 0 && secondNumber >= 0)
+        {
+            score++;
+            allFound++;
+            
+            firstNumber = -1;
+            secondNumber = -1;
+            if(allFound == actualImages.length/2)
+            {  
+                player.score = score;
+                localStorage.setItem("playerInfo", JSON.stringify(player));
+                window.location = "EndPage.html";
+            }
+        }
+    }
+    
+    function imagesDisappear()
+    {
+    
+        console.log(firstNumber);
+        document.getElementById(imageTags[firstNumber]).src = blankImagePath;
+        document.getElementById(imageTags[secondNumber]).src = blankImagePath;
+        firstNumber = -1;
+        secondNumber = -1;
+    }
+    
+    // Note : add to the JSON from the textboxes
+    function addToPlayer()
+    {
+        var firstName = document.getElementById("txtFirstName").value;
+        var lastName = document.getElementById("txtLastName").value;
+        var age = document.getElementById("txtAge").value;
+       
+        player.firstname = firstName;
+        player.lastname = lastName;
+        player.age = age;
+        localStorage.setItem("playerInfo", JSON.stringify(player));
+        window.location = "Index.html";
+    }
+    
+    //  information for JSON
+    function playerInfo()
+    {
+        var playerInformation = localStorage.getItem("playerInfo");
+        player = JSON.parse(playerInformation);
+        var str = "Name: " + player.firstname + " " + player.lastname + "<br>" +
+        "Age: " + player.age + "<br>" +
+        "Score: " + player.score;
+        if(document.getElementById("endInformation") != null)
+        {
+            document.getElementById("endInformation").innerHTML = str;
+        }
+        
+       
+    }
+        
+        
+    
